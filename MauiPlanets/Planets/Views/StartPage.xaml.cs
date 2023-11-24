27cef4e0444e963a94ada19006a4bde905bc41af
Siim Koppel <siimk999@gmail.com>
2023-11-24 11:00:48 +0200

    Add planets img, first planet animation

--- MauiPlanets/Planets/Views/StartPage.xaml.cs
+++ MauiPlanets/Planets/Views/StartPage.xaml.cs
@@ -6,4 +6,23 @@ public partial class StartPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	protected override async void OnAppearing()
+
+	{
+
+		base.OnAppearing();
+
+		if (this.AnimationIsRunning("TransitionAnimation"))
+		{
+			return;
+		}
+		var parentAnimation = new Animation();
+		//Planets animation
+
+		parentAnimation.Add(0.2, 0.4, new Animation(v => imgEarth.Opacity = v, 0, 1, Easing.CubicIn));
+
+		//Intro box
+		parentAnimation.Commit(this, "TransitionAnimation", 16, 3000, null, null);
+	}
 }
