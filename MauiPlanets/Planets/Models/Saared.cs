5356c3bf7e2a233558934a4e86e2245db01830fa
Siim Koppel <siimk999@gmail.com>
2023-12-22 11:40:06 +0200

    Reference comments, todo comments

--- MauiPlanets/Planets/Models/Saared.cs
+++ MauiPlanets/Planets/Models/Saared.cs
@@ -2,7 +2,11 @@
 {
     public class Saared
     {
-        public string HeroImage { get; set; }
+        public string SaarImage { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
         /*public string Name { get; set; }
         public string Subtitle { get; set; }
       
--- MauiPlanets/Planets/Services/eSaaredServices.cs
+++ MauiPlanets/Planets/Services/eSaaredServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//using Planets.;
+using Esaared.Models; // references Models folder
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,22 @@ using System.Threading.Tasks;
 
 namespace Esaared.Services
 {
-    public class eSaaredServices
+    internal static class eSaaredServices
     {
+
+        private static List<Saared> saared = new() // references models folder i think
+        {
+            new()
+            {
+                Name = "Pakri",
+                // Todo fix png reference
+                // Todo fix all references
+                // Todo, move project to Saared project branch
+                //SaarImage = spakri.png,
+                Description = "See on saar"
+
+            }
+        };
 
     }
 }
